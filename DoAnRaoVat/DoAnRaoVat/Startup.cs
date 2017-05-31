4bf1745c1db32eae69a10a36b0948f95deecf6fc
KhaNguyen <KhaNguyen@KHANGUYEN>
2017-05-31 16:54:02 -0700

    Đồ án mới sửa lại giao diện trang user

---
+++ DoAnRaoVat/DoAnRaoVat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAnRaoVat.Startup))]
+namespace DoAnRaoVat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
